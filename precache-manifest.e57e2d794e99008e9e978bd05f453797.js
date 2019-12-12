02108ca7270a0f2ec8d34da3164dca417e2ec771
dcerjan <domagoj.cerjan@gmail.com>
2019-12-12 02:32:52 +0100

    Updates

---
+++ precache-manifest.e57e2d794e99008e9e978bd05f453797.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baab732c99ee68dd5df9061cb2a0b123",
+    "url": "/admash/index.html"
+  },
+  {
+    "revision": "9645544b0eb1dd1d9fa0",
+    "url": "/admash/static/css/2.78b59fc6.chunk.css"
+  },
+  {
+    "revision": "e8b456d229b3eecf9f75",
+    "url": "/admash/static/css/main.aa713225.chunk.css"
+  },
+  {
+    "revision": "9645544b0eb1dd1d9fa0",
+    "url": "/admash/static/js/2.ea9fa292.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/admash/static/js/2.ea9fa292.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8b456d229b3eecf9f75",
+    "url": "/admash/static/js/main.b5ffc081.chunk.js"
+  },
+  {
+    "revision": "1062bad8508ab46c5ad4",
+    "url": "/admash/static/js/runtime-main.832e44ac.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/admash/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/admash/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/admash/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/admash/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/admash/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
